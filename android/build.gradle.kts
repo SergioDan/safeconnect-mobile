3da5b0027fbc0a313e495ca33c369a326eef7b1a
Sergio Ramirez <danielkrad@gmail.com>
2025-12-13 02:30:44 -0800

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -18,5 +18,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.compose.material3:material3:1.2.1")
-    implementation("androidx.compose.ui:ui:1.5.4")
+   
+implementation("androidx.datastore:datastore-preferences:1.1.1")
+ implementation("androidx.compose.ui:ui:1.5.4")
 }
